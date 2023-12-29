a2c333aa52b6ae781de748c9d4613b017df4b1b0
Dominique Stussi <dominique.stussi@ik.me>
2023-12-29 16:17:05 +0100

    intermediate

--- app/lama_secours.py
+++ app/lama_secours.py
@@ -1,9 +1,11 @@
 import uvicorn
+import os
 from fastapi import FastAPI, File, UploadFile
-import zipfile
-import io
-
+from zipfile import ZipFile
+from io import BytesIO
+from uuid import uuid4
 from model import Zip_file
+from datetime import datetime
 
 app = FastAPI()
 
@@ -11,9 +13,28 @@ app = FastAPI()
 def home():
     return {"Message": "Hello World"}
 
-@app.post("/readzip/")
-async def read_zip(file: UploadFile):
-    content = file.read()
-    return content
+@app.post("/uploadzip/")
+async def upload_zip(file: UploadFile):
+    try:
+        #content = BytesIO(file.file.read())
+        content = file.file.read()
+    except Exception:
+        return {"message": "Error with uploading file!"}
+    finally:
+        file.file.close()
+    
+    #zip_file = ZipFile(content)
+    filename = f"{str(uuid4())}.zip"
+    with open(f'tmp/{filename}', 'wb') as f:
+        f.write(content)
+
+    return {"filename": filename}
+
+@app.get("/uploadedfile")
+async def uploaded_file():
+    file_list = os.scandir("tmp")
+    for file in file_list:
+        c_time = datetime.fromtimestamp(os.path.getctime(file))
+        print(f"{file.name}: {c_time.strftime('%Y-%m-%d %H:%M:%S')}")
 if __name__ == "__main__":
     uvicorn.run("lama_secours:app", reload=True)
