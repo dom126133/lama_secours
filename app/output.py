3b7e8ae99d8b3ed216a7ba9c35d5782d7d36e983
Dominique Stussi <dominique.stussi@ik.me>
2024-01-04 22:08:48 +0100

    intermediate

--- app/output.py
+++ app/output.py
@@ -2,6 +2,7 @@ from reportlab.pdfgen import canvas
 from reportlab.platypus import *
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import mm
 import pandas as pd
 import numpy as np
@@ -15,10 +16,17 @@ def pdf(shift_definition):
         task[2] = Paragraph(task[2])
         #print(task[2])
 
+    doc = SimpleDocTemplate("table.pdf", pagesize=A4)
+    styles = getSampleStyleSheet()
+
+    title = Paragraph('Tour XXX', style=styles['h1'])
+    generated_timestamp = Paragraph(f"Generated using data from XXX.", style=styles['Normal'])
     t1 = Table(shift_definition, colWidths=[30*mm,30*mm,140*mm]);
 
-    doc = SimpleDocTemplate("table.pdf", pagesize=A4)
+
     element = []
+    element.append(title)
+    element.append(generated_timestamp)
     element.append(t1)
     doc.build(element)
     return t1
